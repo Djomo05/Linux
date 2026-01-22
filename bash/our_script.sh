57e00bc4f549e334465674b6cf7d570c68cd36d2
Djomo05 <contact@ges-sco.com>
2026-01-22 03:26:03 -0600

    Successfully Completed the Scripting Section

---
+++ bash/our_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p ~/Desktop/Workspace/Linux/bash
+cd ~/Desktop/Workspace/Linux/bash
+touch file{1..100}
+ls -lh ~/Desktop/Workspace/Linux/bash > ~/Desktop/Workspace/Linux/bash/margic.log
